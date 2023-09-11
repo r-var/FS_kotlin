e1042ab3724a01d1004dc3d475d6738304ced8cb
r-var <romanvar91@gmail.com>
2023-09-12 01:44:38 +0300

    FSK-2-1

---
+++ src/main/kotlin/lesson_2_task_1.kt
@@ -0,0 +1,17 @@
+import java.lang.Integer.sum
+
+
+fun main(){
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    print((student1+student2+student3+student4).toDouble()/4)
+
+
+
+}
+
+
+
