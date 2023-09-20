85a2d5e85aad450cd4f082d9c81a1db424bb426c
rvaryanica <romavaryanitsa@yandex.ru>
2023-09-21 02:18:41 +0300

    FSK-3-3

---
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -0,0 +1,19 @@
+package lesson_3
+
+fun main() {
+    val number: Int = 6
+
+    println(
+        """
+    $number х 1 = ${number * 1}
+    $number х 2 = ${number * 2}
+    $number х 3 = ${number * 3}
+    $number х 4 = ${number * 4}
+    $number х 5 = ${number * 5}
+    $number х 6 = ${number * 6}
+    $number х 7 = ${number * 7}
+    $number х 8 = ${number * 8}
+    $number х 9 = ${number * 9}
+    """
+    )
+}
