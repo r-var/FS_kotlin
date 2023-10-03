032237536e6ab4b9b11be740d976982bc399603a
r-var <romanvar91@gmail.com>
2023-10-03 09:29:31 +0300

    FSK-5-1 add sum

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -5,11 +5,11 @@ fun main() {
     val numberFirst = 5
     val numberSecond = 2
 
-    println("Что бы пройти дальше, решите пример $numberFirst+$numberSecond")
+    println("Что бы пройти дальше, решите пример $numberFirst+$numberSecond:")
 
     val number = readln().toInt()
 
-    val wall = if (number == 7) "Добро пожаловать!" else "Доступ запрещен."
+    val wall = if (number == (numberFirst+numberSecond)) "Добро пожаловать!" else "Доступ запрещен."
 
     println(wall)
 
