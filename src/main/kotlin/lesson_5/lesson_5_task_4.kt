fad43483a602433b1f46e57bf8c7482843e0ea6a
r-var <romanvar91@gmail.com>
2023-10-10 01:54:57 +0300

    FSK-5-4 add if else construction

--- src/main/kotlin/lesson_5/lesson_5_task_4.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_4.kt
@@ -1,23 +1,24 @@
 package lesson_5
 
 const val PASSWORD = "PanGalactic"
+const val LOGIN = "Zaphod"
 
 
 fun main() {
 
     println("Введите имя пользователя для входа на корабль:")
 
-    val enterToSpaceShip = when (val name = readln()) {
-        "Zaphod" -> {
-            println("Введите пароль")
-            val enterPass = readln()
-            if (enterPass == PASSWORD) "Добро пожаловать на карабль $name" else "Не верный пароль"
-        }
+    val enterLogin = readln()
 
-        else -> "Вы не зарегистрированы пользователь $name, зарегиструйтесь"
-    }
+    println("Введите пароль пользователя для входа на корабль:")
 
-    println(enterToSpaceShip)
+    val enterPassword = readln()
+
+    val enterSpaceShip = if ((enterLogin == LOGIN) and (enterPassword == PASSWORD)) "Добро пожаловать"
+    else if ((enterLogin == LOGIN) or (enterPassword == PASSWORD)) "Возможно вы ввели не верно логин или пароль"
+    else "Вы $enterLogin не зарегистрированы, пожалуйста зарегистрируйтесь"
+
+    println(enterSpaceShip)
 
 
 }
