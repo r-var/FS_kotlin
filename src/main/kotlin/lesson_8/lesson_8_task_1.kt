97ff13d7c0eee5dc3f58cb405ee293218d79f236
Roman <romanvar91@gmail.com>
2023-10-27 02:13:32 +0300

    FSK-8-1 rename variable

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -1,16 +1,16 @@
 package lesson_8
 
 fun main() {
-    var dayArray = emptyArray<Int>()
+    var viewsArray = emptyArray<Int>()
     var sum = 0
 
 
     for (i in 0..6) {
         println("количество в ${i+1}:")
         val number = readln().toInt()
-        dayArray += number
-        sum += dayArray[i]
+        viewsArray += number
+        sum += viewsArray[i]
 
     }
-    println(sum)
+    println("Просмотров за неделю $sum")
 }
